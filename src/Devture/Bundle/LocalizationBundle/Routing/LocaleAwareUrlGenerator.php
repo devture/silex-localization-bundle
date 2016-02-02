a552f59fe2caaee7c2b9dd94f3ef6e6290b07ba1
Slavi Pantaleev <s.pantaleev@gmail.com>
2016-02-02 15:14:07 +0200

    Unset locale URL parameter for non-locale-aware routes

--- src/Devture/Bundle/LocalizationBundle/Routing/LocaleAwareUrlGenerator.php
+++ src/Devture/Bundle/LocalizationBundle/Routing/LocaleAwareUrlGenerator.php
@@ -36,12 +36,30 @@ class LocaleAwareUrlGenerator extends UrlGenerator {
 
 		$route = $this->routes->get($name);
 		if ($route instanceof Route) {
-			if (strpos($route->getPath(), '{locale}') !== false && !array_key_exists('locale', $parameters)) {
-				try {
-					$parameters['locale'] = $this->getRequest()->getLocale();
-				} catch (\RuntimeException $e) {
-					//Not running in a request context.
+			$isRouteLocaleAware = (strpos($route->getPath(), '{locale}') !== false);
+
+			if ($isRouteLocaleAware) {
+				if (!array_key_exists('locale', $parameters)) {
+					try {
+						$parameters['locale'] = $this->getRequest()->getLocale();
+					} catch (\RuntimeException $e) {
+						//Not running in a request context.
+					}
 				}
+			} else {
+				//Route that doesn't take a locale. If one was provided, get rid of it.
+				//This is necessary, because we put certain systems into single-locale mode
+				//(and transparently remove {locale} from routes.
+				//Still, some code passes locales around.. But the routes no longer need it.
+				//Instead of fixing all these passers so that they don't pass when in single-locale mode,
+				//we do this transparently here.
+				//
+				//Caveat: If someone uses a non-locale-aware route, but wants a `locale` query string,
+				//(`locale` meaning something possibly-unrelated to this library),
+				//we would strip it as well and not pass it along.
+				//This is a known problem. We take over `locale` and don't let people use it when they
+				//include this library.
+				unset($parameters['locale']);
 			}
 		}
 
