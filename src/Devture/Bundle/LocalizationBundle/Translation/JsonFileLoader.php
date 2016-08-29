9b4cb275442fec1348db8a0f5d821f06803d7857
Slavi Pantaleev <s.pantaleev@gmail.com>
2016-08-29 17:08:22 +0000

    Throw friendly exception on unparseable JSON files
    PHP 7 has become a little more picky about JSON parsing,
    which made me stumble into a bug.
    
    Some old file was somewhat-incorrectly encoded
    (contained some long-space character or something),
    which made PHP 7's JSON parser break.
    
    The failure was silently ignored and the translation file
    was not loaded, which shouldn't be the case.

--- src/Devture/Bundle/LocalizationBundle/Translation/JsonFileLoader.php
+++ src/Devture/Bundle/LocalizationBundle/Translation/JsonFileLoader.php
@@ -8,14 +8,21 @@ use Symfony\Component\Config\Resource\FileResource;
 class JsonFileLoader extends ArrayLoader implements LoaderInterface {
 
 	public function load($resource, $locale, $domain = 'messages') {
-		$messages = json_decode(file_get_contents($resource), 1);
+		$contents = file_get_contents($resource);
 
-		// empty file
-		if (null === $messages) {
-			$messages = array();
+		$messages = array();
+		if ($contents !== '') {
+			$messages = json_decode($contents, 1);
+
+			if (json_last_error() !== JSON_ERROR_NONE) {
+				throw new \InvalidArgumentException(sprintf(
+					'The file "%s" cannot be JSON-decoded: %s.',
+					$resource,
+					json_last_error_msg()
+				));
+			}
 		}
 
-		// not an array
 		if (!is_array($messages)) {
 			throw new \InvalidArgumentException(sprintf('The file "%s" must contain a JSON array.', $resource));
 		}
